21c6f2e03c61ec7fe2f33f1ec0940d4dc176399f
abkeshvari <abolfazl.keshvari@aalto.fi>
2016-02-01 16:19:31 +0200

    Update Normalization.m

--- Normalization.m
+++ Normalization.m
@@ -1,4 +1,6 @@
 function [ D ] = Normalization( w,tau)
+%%% Please cite the paper properly if you use the code. 
+%%% "Keshvari, Abolfazl. 2016. An Enhanced Fourier-Motzkin Method for DEA."
 D=zeros(1,tau);
 for i=1:tau
     if w(i)~=0
